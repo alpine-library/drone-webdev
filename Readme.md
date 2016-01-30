47f1654bd3ddb12ed3681c340d8857c16c5effb9
Administrator <m@3ko.fr>
2016-01-30 20:24:05 +0100

    .

---
+++ Readme.md
@@ -0,0 +1,11 @@
+#WebDev Dockerfile
+
+Just a small image powered with alpine linux optimized for drone :)
+
+Inside :
+  - Git
+  - Go 1.5
+  - Node
+  - Bower
+  - Grunt
+  - docker
---
+++ ci.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+name='webdev'
+namespace='alpinelib'
+
+declare -A aliases
+aliases=(
+	["1.0"]='1 latest'
+)
+
+versions=( */ )
+versions=( "${versions[@]%/}" )
+
+for version in "${versions[@]}"; do
+
+  fullVersion="$(grep -m1 'ENV WEBDEV_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
+
+	versionAliases=()
+
+	while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
+		versionAliases+=( $fullVersion )
+		fullVersion="${fullVersion%[.-]*}"
+	done
+
+	versionAliases+=( $version ${aliases[$version]} )
+
+  cd $version
+  echo "build docker image $version"
+  ID=$(docker build .  | tail -1 | sed 's/.*Successfully built \(.*\)$/\1/')
+  cd ..
+
+	for va in "${versionAliases[@]}"; do
+    echo "TAG image $ID -> $namespace/$name:$va"
+    docker tag -f $ID $namespace/$name:$va
+    if [ $DOCKER_PUSH ]; then
+    echo "PUSH image $ID -> $namespace/$name:$va"
+    docker push $namespace/$name:$va
+    fi
+	done
+done
